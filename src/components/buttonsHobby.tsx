3156534814e572abaa06b81ed34a5dfaaedbf3e7
Artem Rabets <artemrabets228777@gmail.com>
2025-10-12 15:28:16 +0300

    Work Buttons Category in Profile

--- src/components/buttonsHobby.tsx
+++ src/components/buttonsHobby.tsx
@@ -1,12 +1,26 @@
 export function ButtonsElements() {
   const hobbyButtons = document.querySelectorAll(".hobbyButton");
-  const result = [];
+  const handleButtonHobby: any = [];
   for (const element of hobbyButtons) {
-    result.push(element);
+    let isButtonActive = false;
     element.addEventListener("click", (e) => {
       e.preventDefault();
-      element.classList.add("border-rose-800", "bg-neutral-900");
-      console.log(element.innerHTML);
+      if (isButtonActive) {
+        handleButtonHobby.pop(element.id);
+        element.classList.remove("shadow-rose-100", "border-rose-900");
+        isButtonActive = false;
+      } else {
+        element.classList.add(
+          "px-2",
+          "py-2",
+          "rounded-xl",
+          "shadow-rose-100",
+          "border-2",
+          "border-rose-900"
+        );
+        handleButtonHobby.push(element.id);
+        isButtonActive = true;
+      }
     });
   }
   return (
@@ -14,52 +28,100 @@ export function ButtonsElements() {
       <div className="flex flex-col gap-5">
         <h1>Hobby</h1>
         <div className="grid grid-cols-2 gap-2 w-80 h-7">
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="footbol"
+          >
             footbol
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
-            swimmming
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="swimming"
+          >
+            swimming
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="basketboll"
+          >
             basketball
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="knitting"
+          >
             knitting
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="embroidery"
+          >
             embroidery
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="modeling"
+          >
             modeling
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="reading"
+          >
             reading
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="cooks"
+          >
             cooks
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="drawind"
+          >
             drawing
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="photography"
+          >
             photography
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="dance"
+          >
             dance
           </button>
-          <button className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="camping"
+          >
             camping
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="singleCombat"
+          >
             single combat
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="plaingComputerGame"
+          >
             plaing computer game
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="singing"
+          >
             singing
           </button>
-          <button className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="hobbyButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="otherSports"
+          >
             other sports
           </button>
         </div>
--- src/components/habits.tsx
+++ src/components/habits.tsx
@@ -1,12 +1,27 @@
 export function HabitsElements() {
   const habitsButtons = document.querySelectorAll(".habitsButton");
-  const result = [];
+  const handleButtonHabits: any = [];
   for (const element of habitsButtons) {
-    result.push(element);
+    let isButtonActive = false;
+
     element.addEventListener("click", (e) => {
       e.preventDefault();
-      element.classList.add("border-rose-800", "bg-neutral-900");
-      console.log(element.innerHTML);
+      if (isButtonActive) {
+        handleButtonHabits.pop(element);
+        element.classList.remove("shadow-rose-100", "border-rose-900");
+        isButtonActive = false;
+      } else {
+        element.classList.add(
+          "px-2",
+          "py-2",
+          "rounded-xl",
+          "shadow-rose-100",
+          "border-2",
+          "border-rose-900"
+        );
+        handleButtonHabits.push(element.id);
+        isButtonActive = true;
+      }
     });
   }
   return (
@@ -14,28 +29,52 @@ export function HabitsElements() {
       <div className="flex flex-col gap-5 mt-100">
         <h1>Habits</h1>
         <div className="grid grid-cols-2 gap-2 w-80 h-7">
-          <button className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="balancedDiet"
+          >
             Balanced Diet
           </button>
-          <button className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="maintainingSocialConnections"
+          >
             Maintaining Social Connections
           </button>
-          <button className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="sufficientSleep"
+          >
             Sufficient Sleep
           </button>
-          <button className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="stressManagement"
+          >
             Stress Management
           </button>
-          <button className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="smoking"
+          >
             Smoking
           </button>
-          <button className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="excessiveAlcoholConsumption"
+          >
             Excessive Alcohol Consumption
           </button>
-          <button className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton   px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="sedentaryLifestyle"
+          >
             Sedentary Lifestyle
           </button>
-          <button className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600">
+          <button
+            className="habitsButton  px-2 py-2 rounded-xl shadow-rose-800 border-2 border-rose-600"
+            id="compulsiveSpending"
+          >
             Compulsive Spending
           </button>
         </div>
